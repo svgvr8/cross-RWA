3991eb13d2435d53b713e9cf05780935c2bbc13b
Jeremy Boetticher <me4jeremyboett@gmail.com>
2025-01-20 16:28:32 +0800

    chore: removed boilerplate

---
+++ script/DeployXERC20.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.20;
+
+import "forge-std/Script.sol";
+import {XERC20Basic} from "v1-core/contracts/token/XERC20.sol";
+
+/** GOALS
+
+1. Deploy a new xERC20 token
+2. Deploy multiple SimpleToken
+
+*/
+
+contract DeployXERC20 is Script {
+    
+}
