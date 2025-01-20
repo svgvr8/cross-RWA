46e86f3974ac90ca6a720548dc18c989ad27bcaf
Jeremy Boetticher <me4jeremyboett@gmail.com>
2025-01-20 17:19:26 +0800

    chore: removed openzeppelin 4.5.2

--- script/DeployXERC20.sol
+++ script/DeployXERC20.sol
@@ -1,13 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.20;
 
+import {XERC20Basic} from "@glacis/contracts/token/XERC20.sol";
+import {SimpleTokenMediator} from "@glacis/contracts/mediators/SimpleTokenMediator.sol";
 import "forge-std/Script.sol";
-import {XERC20Basic} from "v1-core/contracts/token/XERC20.sol";
 
 /** GOALS
 
 1. Deploy a new xERC20 token
-2. Deploy multiple SimpleToken
+2. Deploy multiple SimpleTokenMediator contracts with different configurations
+3. Set the xERC20 token to use the SimpleTokenMediator
+4. Send through multiple pathways
+
+In documentation, explain why this is easier & how its done.
 
 */
 
