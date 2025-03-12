bed43b020fa01768f0b9ddc10e2de8f5cb71a807
Jeremy Boetticher <me4jeremyboett@gmail.com>
2025-03-11 23:52:55 -0500

    feat: created calculate gas price function

--- script/SendToken.sol
+++ script/SendToken.sol
@@ -4,38 +4,56 @@ pragma solidity 0.8.20;
 import {BasicXERC20Sample} from "@glacis/test/contracts/samples/token/BasicXERC20Sample.sol";
 import {SimpleTokenMediator} from "@glacis/contracts/mediators/SimpleTokenMediator.sol";
 import {GlacisCommons} from "@glacis/contracts/commons/GlacisCommons.sol";
+import {IWormholeRelayer} from "@glacis/contracts/adapters/Wormhole/IWormholeRelayer.sol";
 import "forge-std/Script.sol";
 
 contract SendToken is Script, GlacisCommons {
     string constant CHAIN_TO_RUN_ON = "arbitrum";
-    address constant GMP_CHAIN_ID = address(3);
+    address constant GMP_CHAIN_ID = address(4);
     uint256 constant DEST_CHAIN_ID = 10;
     address constant XERC20 = 0x75B6AAEaF6DB9F2F5DFF09bc5ca6954c34Bd9fea;
-    address constant SIMPLE_TOKEN_MEDIATOR = 0xC23436e54fEBF9cFdD5FDaD41142Ac7B706b8684;
-    uint256 constant AMOUNT_TO_SEND = 100000;
-
-    uint256 constant CROSS_CHAIN_GAS = 550_000_000_000_000;
+    address constant SIMPLE_TOKEN_MEDIATOR =
+        0xC23436e54fEBF9cFdD5FDaD41142Ac7B706b8684;
 
     // string constant CHAIN_TO_RUN_ON = "optimism";
-    // address constant GMP_CHAIN_ID = address(3);
+    // address constant GMP_CHAIN_ID = address(5);
     // uint256 constant DEST_CHAIN_ID = 42161;
     // address constant XERC20 = 0x75B6AAEaF6DB9F2F5DFF09bc5ca6954c34Bd9fea;
     // address constant SIMPLE_TOKEN_MEDIATOR = 0xa54B373A9e8305604F66836396073eE24ab09322;
+
+    // You can find destination gas by sending a transaction to a destination chain and checking the gas used.
+    // In this example, we are overestimating the gas used for safety, simplicity, & discrepencies between chains.
+    // https://optimistic.etherscan.io/tx/0x7176aa57d336ba1f97e61944ae96bf954a7329e2ff2d0596aa39e1f3eb14af11
+    uint256 constant DESTINATION_CHAIN_GAS = 280000;
+    uint256 constant AMOUNT_TO_SEND = 100;
 
     function run() external {
         vm.createSelectFork(CHAIN_TO_RUN_ON);
         vm.startBroadcast(tx.origin);
 
-        // Now send a token from arbitrum to optimism
-        BasicXERC20Sample(XERC20).approve(SIMPLE_TOKEN_MEDIATOR, AMOUNT_TO_SEND);
+        // Approve the SimpleTokenMediator with the token to be sent
+        BasicXERC20Sample(XERC20).approve(
+            SIMPLE_TOKEN_MEDIATOR,
+            AMOUNT_TO_SEND
+        );
+
+        // Calculate cross-chain gas
+        uint128 gasCost = _calculateCrossChainGasPrice(
+            DESTINATION_CHAIN_GAS,
+            DEST_CHAIN_ID,
+            GMP_CHAIN_ID
+        );
+        CrossChainGas[] memory fees = new CrossChainGas[](1);
+        fees[0] = CrossChainGas(uint128(DESTINATION_CHAIN_GAS), gasCost);
+
+        // Set the adapter (bridge) to be used
         address[] memory adapters = new address[](1);
         adapters[0] = GMP_CHAIN_ID;
-        CrossChainGas[] memory fees = new CrossChainGas[](1);
-        fees[0] = CrossChainGas(
-            0,
-            uint128(CROSS_CHAIN_GAS)
-        );
-        SimpleTokenMediator(SIMPLE_TOKEN_MEDIATOR).sendCrossChain{ value: CROSS_CHAIN_GAS  }(
+
+        // Use the simple token mediator to send across chains
+        SimpleTokenMediator(SIMPLE_TOKEN_MEDIATOR).sendCrossChain{
+            value: gasCost
+        }(
             DEST_CHAIN_ID,
             bytes32(uint256(uint160(address(tx.origin)))),
             adapters,
@@ -47,20 +65,49 @@ contract SendToken is Script, GlacisCommons {
         vm.stopBroadcast();
     }
 
-    IWormholeRelayer constant WORMHOLE_RELAYER = IWormholeRelayer(0x27428DD2d3DD32A4D7f7C497eAaa23130d894911);
+    IWormholeRelayer constant WORMHOLE_RELAYER =
+        IWormholeRelayer(0x27428DD2d3DD32A4D7f7C497eAaa23130d894911);
+    ILayerZeroEndpoint constant LAYERZERO_ENDPOINT = 
+        ILayerZeroEndpoint(0x1a44076050125825900e736c501f859c50fE728c);
 
-    function _calculateCrossChainGasPrice(uint256 gas, uint256 dstChain, address gmp) internal view returns (uint256) {
-        
-
+    function _calculateCrossChainGasPrice(
+        uint256 gas,
+        uint256 dstChain,
+        address gmp
+    ) internal view returns (uint128) {
+        // For LayerZero
+        if (gmp == address(2)) {
+            // Optimism: 
+            // Arbitrum: 0x1a44076050125825900e736c501f859c50fE728c
+            LAYERZERO_ENDPOINT.estimateFees(
+                _dstEid, 
+                _sender, 
+                _message, 
+                false, // _payInLzToken 
+                _options
+            );
+            return 10000000;
+        }
         // For wormhole
         if (gmp == address(3)) {
-            WORMHOLE_RELAYER.quoteEVMDeliveryPrice(
-                101, // TODO: map chain ID to Wormhole chain ID
+            (uint256 nativePriceQuote, ) = WORMHOLE_RELAYER.quoteEVMDeliveryPrice(
+                // Arbitrum Wormhole ChainID is 23. Optimism Wormhole ChainID is 24.
+                // In a real application, a much larger mapping would be available & easier to access.
+                dstChain == 42161 ? 23 : 24,
                 0,
                 gas
             );
+            return uint128(nativePriceQuote);
         }
-        // For layerzero (TODO)
+        // For hyperlane (TODO)
+        if (gmp == address(5)) {
+            return 550_000_000_000_000;
+        }
+        // For others, we can guesstimate
         return 200_000_000_000_000;
     }
+}
+
+interface ILayerZeroEndpoint {
+    function estimateFees(uint16 _dstEid, address _sender, bytes _message, bool _payInLzToken, bytes _options) external view returns (uint256 nativeFee, uint256 lzTokenFee);
 }
